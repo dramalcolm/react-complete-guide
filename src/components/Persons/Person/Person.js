0add18ac4f755dca46b6c647bb75796936e0e7ab
Douglas A. Malcolm <douglas@876synergy.com>
2018-08-09 15:06:01 -0400

    Using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import PropTypes from 'prop-types';
 import cssPerson from './Person.css'; //Scoping css to component import "CSS Module"
 import withClass from '../../../hoc/WithClass';
 import Aux from '../../../hoc/Aux';
@@ -24,14 +25,21 @@ class Person extends Component {
     render (){
         return(
         <Aux>
-        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
         </Aux>
         )
     }
 };
 
+Person.propTypes = {
+    click: PropTypes.func,
+    name:PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
+//install to validate type or property: npm install --save prop-types
 //export default Radium(person); removed to be used CSS Module
 export default withClass(Person,cssPerson.Person);
