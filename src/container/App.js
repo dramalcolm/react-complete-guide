e251aa3a86614446a07ba93ffc0a5f907b714f33
Douglas A. Malcolm <douglas@876synergy.com>
2018-08-13 20:55:38 -0500

    improvements

--- src/container/App.js
+++ src/container/App.js
@@ -36,6 +36,7 @@ class App extends PureComponent {
     console.log('[App.js] inside componentWillMount()');
   }
   */
+ 
   componentDidMount(){
     console.log('[App.js] inside componentDidMount()');
   }
